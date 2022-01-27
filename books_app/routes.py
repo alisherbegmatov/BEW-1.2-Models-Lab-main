dabf2f51fc01a92688ab8902da2840b1d664a447
alisherbegmatov <alisher.begmatov@students.dominican.edu>
2022-01-26 19:58:27 -0800

    Initial commit

---
+++ books_app/routes.py
@@ -0,0 +1,29 @@
+"""Import packages and modules."""
+import os
+from flask import Blueprint, request, render_template, redirect, url_for, flash
+from datetime import date, datetime
+from books_app.models import User, Book, Author, Genre
+
+# Import app and db from events_app package so that we can run app
+from books_app import app, db
+
+main = Blueprint("main", __name__)
+
+##########################################
+#           Routes                       #
+##########################################
+
+
+@main.route("/")
+def homepage():
+    users = User.query.all()
+    return render_template("home.html", users=users)
+
+
+@main.route("/profile/<username>")
+def profile(username):
+    user = User.query.filter_by(username=username).one()
+    favorite_books = user.favorite_books
+    return render_template(
+        "profile.html", username=username, favorite_books=favorite_books
+    )
